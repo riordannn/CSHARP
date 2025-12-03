71c111464b941ab2fa50a1a7c53b9a5db4a03444
riordannn <riordan9001@gmail.com>
2025-12-03 12:49:10 -0300

    Add files via upload

---
+++ 02 - programação orientada a objetos/dia-01/02-anatomia-classe/_assets/01-exercicio1-boilerplate.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+
+// ----------------------------
+// Classe que representa um registro de extrato
+// ----------------------------
+namespace Boilerplate
+{
+    public class Movimentacao
+    {
+        public string CpfPessoa;        // CPF da pessoa (para referência)
+        public DateTime DataInclusao;   // Data da transação
+        public double ValorTransacao;
+
+        // Construtor
+        public Movimentacao(string cpf, DateTime data, double valor)
+        {
+            CpfPessoa = cpf;
+            DataInclusao = data;
+            ValorTransacao = valor;
+        }
+    }
+
+    // ----------------------------
+    // Classe que representa o extrato
+    // ----------------------------
+    public class ExtratoBancario
+    {
+        public string CpfPessoa;       // CPF da pessoa
+        public string NomePessoa;      // Nome da pessoa
+        public double ValorInicial;    // Saldo inicial
+        protected List<Movimentacao> Movimentacoes;
+
+        // Construtor
+        public ExtratoBancario(string cpf, string nome, double valorInicial, List<Movimentacao> movimentacoes)
+        {
+            CpfPessoa = cpf;
+            NomePessoa = nome;
+            ValorInicial = valorInicial;
+            Movimentacoes = movimentacoes ?? new List<Movimentacao>();
+        }
+
+        // ----------------------------
+        // Método para calcular saldo final
+        // ----------------------------
+        public double CalcularSaldo()
+        {
+            // TODO: Implementar cálculo do saldo final
+            return 0;
+        }
+
+        // ----------------------------
+        // Método para obter saldos diários
+        // ----------------------------
+        public List<string> ObterSaldosDiarios()
+        {
+            // TODO: Implementar cálculo de saldo diário
+            return new List<string>();
+        }
+    }
+
+    // ----------------------------
+    // Classe App com método Main
+    // ----------------------------
+    public class App
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("=== Sistema de Extrato Bancário ===\n");
+
+            // TODO: Instanciar ExtratoBancario e Movimentacoes conforme o exercício
+            // Exemplo de comentário:
+            // var movimentacoes = new List<Movimentacao> { ... };
+            // var extrato = new ExtratoBancario("CPF", "Nome", 1000, movimentacoes);
+
+            // TODO: Chamar CalcularSaldo e ObterSaldosDiarios, imprimir resultados
+            Console.WriteLine("\nFim do programa. Implemente as funcionalidades solicitadas no exercício.");
+        }
+    }
+
+}
